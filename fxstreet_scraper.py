7bc51ba75223c312b18c72ec7226f242eb89362b
kkjang <kevinjang7@gmail.com>
2016-09-15 20:52:06 -0700

    add scraper for fxstreet

---
+++ fxstreet_scraper.py
@@ -0,0 +1,32 @@
+import requests
+
+# Hardcoded currencies, to be changed
+
+def main():
+	return get_csv()
+
+def get_csv():
+	headers = {
+	'Host': 'calendar.fxstreet.com',
+	'Connection': 'keep-alive',
+	'Pragma': 'no-cache',
+	'Cache-Control': 'no-cache',
+	'Upgrade-Insecure-Requests': '1',
+	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	'Referer': 'http://www.fxstreet.com/economic-calendar',
+	'Accept-Encoding': 'gzip, deflate, sdch, br',
+	'Accept-Language': 'en-US,en;q=0.8,zh-TW;q=0.6'
+	}
+	url = "https://calendar.fxstreet.com/eventdate/?f=csv&v=2&timezone=UTC&rows=&view=range&start=20160915&end=20160915&countrycode=AU%2CCA%2CCN%2CEMU%2CFR%2CDE%2CGR%2CIT%2CJP%2CNZ%2CPT%2CES%2CCH%2CUK%2CUS&volatility=0&culture=en&columns=CountryCurrency%2CCountdown"
+	csv_data = requests.get(url, headers=headers)
+	csv_encode = csv_data.text.encode('utf-8')
+	
+	with open('event_calendar.csv', 'w') as csv_file:
+		for row in csv_encode.split('/r/n'):
+			csv_file.write(row)
+
+	return csv_data
+
+if __name__ == "__main__":
+	main()
