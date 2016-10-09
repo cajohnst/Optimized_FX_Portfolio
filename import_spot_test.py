0d26b1ec2f41d9aef4cf11ff12693fcef0aa1625
cajohnst <cajohnst1@gmail.com>
2016-10-08 20:38:10 -0400

    forgot this one!

---
+++ import_spot_test.py
@@ -0,0 +1,53 @@
+# import pandas.io.data as web
+# from pandas.io.data import DataReader
+import datetime
+from datetime import timedelta, date
+# import io
+# import requests
+# import pandas as pd
+import csv
+import requests
+import pandas as pd 
+import StringIO
+
+def main():
+
+	currency_list = get_currency_list()
+
+	yahoo_list = ['MXN=X', 'USDCAD=X', 'NZDUSD=X', 'HKD=X', 'USDJPY=X', 'USDSGD=X', 'GBPUSD=X', 'USDZAR=X', 'AUDUSD=X', 'EURUSD=X']
+	live_quotes = None 
+	for ticker in yahoo_list:
+		url = 'http://chartapi.finance.yahoo.com/instrument/1.0/{0}/chartdata;type=quote;range=1d/csv'.format(ticker)
+
+		with requests.Session() as s:
+			download = s.get(url)
+
+			decoded_content = download.content.decode('utf-8')
+
+			csv_buffer = StringIO.StringIO(decoded_content)
+			yahoo_finance_data = pd.read_csv(csv_buffer, skiprows= 18, header= None, index_col= False)
+			yahoo_finance_data[0] = pd.to_datetime(yahoo_finance_data[0], unit= 's')
+			yahoo_finance_data[0] = yahoo_finance_data[0].apply(lambda x:x.date())
+			yahoo_finance_data[0] = pd.to_datetime(yahoo_finance_data[0])
+			yahoo_finance_data = yahoo_finance_data.iloc[-1][:2]
+
+			live_quote = pd.DataFrame(yahoo_finance_data)
+			live_quote = live_quote.transpose()
+			live_quote = live_quote.set_index(0)
+
+		if live_quotes is None:
+			live_quotes = live_quote
+		else:
+			live_quotes = live_quotes.join(live_quote, how= 'left', rsuffix= ' ')
+	live_quotes.columns = currency_list 
+	live_quotes.index.names = ['DateTime']
+
+	return live_quotes
+
+def get_currency_list():
+	currency_list = ['USD/MXN', 'USD/CAD', 'NZD/USD', 'USD/HKD', 'USD/JPY', 'USD/SGD', 'GBP/USD', 'USD/ZAR', 'AUD/USD', 'EUR/USD']
+	return currency_list 
+
+
+if __name__ == "__main__":
+	main()
